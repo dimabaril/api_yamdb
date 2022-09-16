0214ccd42118916c4c0364ad736b3a9424dfc4f1
Dmitrii Barilkin <dimabaril@gmail.com>
2022-09-16 16:08:06 +0400

    group-project-copied

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# api_yamdb
-api_yamdb
+###YamDB API
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Documentation](#Documentation)
+## General info
+This project is simple social network about reviewing all titles in the workd :smiley_cat: with API.
+	
+## Technologies
+Project is created with:
+ * requests==2.26.0
+ * django==2.2.16
+ * djangorestframework==3.12.4
+ * PyJWT==2.1.0
+ * pytest==6.2.4
+ * pytest-django==4.4.0
+ * pytest-pythonpath==0.7.3
+ * djangorestframework-simplejwt==4.7.2
+ * django-filter==21.1
+
+	
+## Setup
+To run this project, install it locally using npm:
+
+```
+$ cd ../api_yamdb
+$ py venv vnev
+$ . venv/Scrpits/Activate (. bin/Scripts/Activate)
+$ pip install -r 'requarements.txt'
+$ cd api_yamdb
+$ py manage.py runserver
+
+**optianly you can import DB from csv files**
+**type py manage.py importcsv --help for more info**
+
+$ py manage.py importcsv
+```
+
+## Documentation
+You can find all paths with description in .../redoc
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,18 +1,15 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from datetime import timedelta
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = '8817z2uh)+s(f*rrww88gf(6$xrre0g06am!j27ijiiiito9px'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -21,6 +18,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'django_filters',
+    'users',
+    'api',
+    'reviews',
 ]
 
 MIDDLEWARE = [
@@ -35,11 +38,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'api_yamdb.urls'
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -55,8 +57,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 
-# Database
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -64,8 +64,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -83,8 +81,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -96,8 +92,33 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=180),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+AUTH_USER_MODEL = 'users.User'
+
+RESERVED_NAME = 'me'
+MESSAGE_FOR_RESERVED_NAME = 'Имя пользователя "me" использовать нельзя!'
+
+MESSAGE_FOR_USER_NOT_FOUND = 'Пользователя с таким именем нет!'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'webmaster@localhost'
